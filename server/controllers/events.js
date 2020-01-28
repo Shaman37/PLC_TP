d92a7e346cf2f202bf6fc855f857b0aa8cfda49c
Manuel <manueljoao1996@hotmail.com>
2020-01-28 00:20:30 +0000

    fix

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -78,9 +78,3 @@ module.exports.remove = id => {
         .deleteOne({ _id: id })
         .exec()
 }
-
-module.exports.removeMany = ids => {
-    return Event
-        .deleteOne({ _id: { $in: id } })
-        .exec()
-}
